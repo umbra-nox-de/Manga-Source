3ca16ab0888b9f9bd19f9d2716872609aafeeafc
umbra-nox-de <nhsk64hfdf@privaterelay.appleid.com>
2026-09-07 23:19:28 -0400

    feat: export core modules

---
+++ src/core/index.ts
@@ -0,0 +1,5 @@
+export * from "./models/manga.js";
+export * from "./models/chapter.js";
+export * from "./models/page.js";
+export * from "./models/source.js";
+export * from "./source-manager.js";
